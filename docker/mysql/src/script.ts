b7afa1bfccd41de903f08d14aa503da77879f396
YuyaIida <i12.yuya.i15@gmail.com>
2022-07-24 18:37:42 +0900

    fix: express server

--- docker/mysql/src/script.ts
+++ docker/mysql/src/script.ts
@@ -2,18 +2,9 @@ import { PrismaClient } from '@prisma/client'
 
 const prisma = new PrismaClient()
 
-const main = async () => {
-    // const user = await prisma.company.create({
-    //     data: {
-    //         name: 'Harley-Davidson'
-    //     }
-    // })
+export const fetchCompany = async () => {
 
     const users = await prisma.company.findMany()
 
     console.log(users)
 }
-
-main()
-    .catch((e) => { throw e })
-    .finally(async () => prisma.$disconnect())
--- docker/mysql/src/server.ts
+++ docker/mysql/src/server.ts
@@ -1,5 +1,6 @@
 'use strict'
 const express = require('express');
+import { fetchCompany } from './script'
 
 const PORT = 8080
 const HOST = '0.0.0.0'
@@ -8,6 +9,11 @@ const app = express()
 
 app.get('/', (req: any, res: any) => {
     res.send('Hello world')
+})
+
+app.get('/company', (req: any, res: any) => {
+    fetchCompany()
+        .catch((e) => { throw e })
 })
 
 app.listen(PORT, HOST)
